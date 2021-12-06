761e01d2adbc7d7d736acb8e7a2454a9d8f7ffc0
Eli-WL <wind25bear26@gmail.com>
2021-12-06 23:36:29 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const exphbs = require('express-handlebars');
 
 // mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect('mongodb://localhost/todo-list')
@@ -15,8 +16,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  // res.send('hello world!')
+  res.render('index')
 })
 
 
