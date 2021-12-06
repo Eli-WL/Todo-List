ed20603d3d621afcc455e1f874b351dc0fdda774
Eli-WL <wind25bear26@gmail.com>
2021-12-07 00:02:03 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const app = express()
 const exphbs = require('express-handlebars');
 
+const Todo = require('./models/todo') // 載入 Todo model
+
 // mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect('mongodb://localhost/todo-list')
 
@@ -19,11 +21,17 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
-  // res.send('hello world!')
-  res.render('index')
-})
+// app.get('/', (req, res) => {
+//   // res.send('hello world!')
+//   res.render('index')
+// })
 
+app.get('/', (req, res) => {
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+})
 
 app.listen(3000, () => {
   console.log('App is running on port 3000')
