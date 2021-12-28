2b9c192da3965335b8b8c39c6298689f969e8a69
Eli-WL <wind25bear26@gmail.com>
2021-12-29 00:29:57 +0800

    bug fixed_5 mongo string error

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONOGODB_URI || 'mongodb://localhost/todo-list'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 // mongoose.connect('mongodb://localhost/todo-list')
